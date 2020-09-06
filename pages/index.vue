afcb35bc8107037590033c55de64424f28e9c957
Sarah Paz <sarah.paz@gmail.com>
2020-09-06 00:23:47 -0400

    deploy application

---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">nuxt-fundamentals</h1>
+      <h2 class="subtitle">My gnarly Nuxt.js project</h2>
+      <div class="links">
+        <nuxt-link
+          v-for="post in posts"
+          :key="post.id"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          class="button--grey"
+        >{{post.title}}</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+export default {
+  component: {
+    Logo,
+  },
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        { name: "twitter:title", content: "Nuxt Fundamentals by Vue School" },
+        { name: "twitter:description", content: "Nuxt + VUe School" },
+        { name: "twitter:image", content: "https://i.imgur.com/UYP2umJ.png" },
+        { name: "twitter:card", content: "summary_large_image" },
+      ],
+    };
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts.all;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 100;
+  font-size: 100px;
+  color: var(--yellow);
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
