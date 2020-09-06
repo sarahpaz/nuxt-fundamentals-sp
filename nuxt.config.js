7ec16f021426e2cc7863ef3a666e1345f6da4477
Sarah Paz <sarah.paz@gmail.com>
2020-09-06 00:43:38 -0400

    generate routes for netlify deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,11 @@ export default {
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
+
+  generate: {
+    routes: ["/posts/balut", "/posts/whereIsIt", "/posts/how"]
+  },
+
   head: {
     title: process.env.npm_package_name || "",
     meta: [
